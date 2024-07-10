27f178e277fba9313869321b300ec529a4f6c993
Yaroslav781 <pelexach12@gmail.com>
2024-07-10 17:05:57 +0300

    created

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {    
+    slidesPerView: "auto",
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  }
+);
